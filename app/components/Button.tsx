9dd54593b700d0a070998a8eeeddea5929bf72f4
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-10-21 19:09:57 +0600

    Build PageHeader

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,15 +2,23 @@ import { cva, VariantProps } from "class-variance-authority";
 import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
-const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors"], {
+export const buttonStyles = cva(["transition-colors"], {
   variants: {
     variant: {
-      default: ["bg-secondary", "hover:bg-gray-100"],
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
       ghost: ["hover:bg-gray-100"],
     },
     size: {
-      default: ["rounded", "p-2"],
-      icon: ["rounded-full", "w-10", "h-10", "flex", "items-center", "p-2.5"],
+      default: [" rounded", "p-2"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "p-2.5",
+      ],
     },
   },
   defaultVariants: {
--- app/layouts/PageHeader.tsx
+++ app/layouts/PageHeader.tsx
@@ -1,11 +1,20 @@
-import { Menu } from "lucide-react";
+"use client";
+
+import { Bell, Menu, Mic, Search, Upload, User, ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import Button from "@/app/components/Button";
+import { useState } from "react";
 
 const PageHeader = () => {
+  const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
+
   return (
-    <header className="flex justify-between gap-10 lg:gap-20">
-      <div className="flex items-center gap-4 flex-shrink-0">
+    <header className="flex justify-between gap-8 lg:gap-16 pt-2 mb-6 px-4 items-center">
+      <div
+        className={` ${
+          showFullWidthSearch ? "hidden" : "flex"
+        }  items-center gap-4 flex-shrink-0`}
+      >
         <Button size="icon" variant={"ghost"}>
           <Menu />
         </Button>
@@ -47,8 +56,64 @@ const PageHeader = () => {
           </svg>
         </Link>
       </div>
-      <div>sdf</div>
-      <div>sdf</div>
+
+      {showFullWidthSearch && (
+        <Button
+          onClick={() => setShowFullWidthSearch(false)}
+          size="icon"
+          variant={"ghost"}
+        >
+          <ArrowLeft />
+        </Button>
+      )}
+
+      <form
+        className={`${
+          showFullWidthSearch ? "flex" : "hidden md:flex"
+        }   gap-4 flex-grow justify-center`}
+      >
+        <div className="flex items-center flex-grow max-w-[600px]">
+          <input
+            type="search"
+            placeholder="Search"
+            className="rounded-l-full border border-secondary  shadow-inner shadow-secondary py-1 px-4 text-lg w-full outline-none focus:border-blue-500"
+          />
+          <Button className="py-[6px] px-4 rounded-r-full border border-secondary border-l-0 flex-shrink-0">
+            <Search />
+          </Button>
+        </div>
+        <div>
+          <Button type="button" className="flex-shrink-0 " size="icon">
+            <Mic />
+          </Button>
+        </div>
+      </form>
+      <div
+        className={`${
+          showFullWidthSearch ? "hidden" : "flex"
+        } items-center flex-shrink-0 gap-2`}
+      >
+        <Button
+          onClick={() => setShowFullWidthSearch(true)}
+          className="md:hidden"
+          size="icon"
+          variant={"ghost"}
+        >
+          <Search />
+        </Button>
+        <Button variant={"ghost"} className=" md:hidden" size="icon">
+          <Mic />
+        </Button>
+        <Button size="icon" variant={"ghost"}>
+          <Upload />
+        </Button>
+        <Button size="icon" variant={"ghost"}>
+          <Bell />
+        </Button>
+        <Button size="icon" variant={"ghost"}>
+          <User />
+        </Button>
+      </div>
     </header>
   );
 };
