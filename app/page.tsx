2e663e5a3c6c46cc9eb245517221a073ece32d26
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-10-22 09:15:16 +0600

    Build Video Grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,34 @@
 "use client";
 import Categories from "@/app/components/Categories";
 import PageHeader from "./layouts/PageHeader";
-import { categories } from "./data/home";
+import { categories, videos } from "./data/home";
 import { useState } from "react";
+import { VideoGridItem } from "./components/VideoGridItem";
+import SideBar from "./layouts/SideBar";
 
 export default function Home() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
   return (
-    <main className="flex flex-col max-h-screen">
+    <div className="max-h-screen flex flex-col">
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
-        <div>SideBar</div>
-        <div className="overflow-x-hidden  pb-4 px-4 ">
-          <div className="sticky top-0 bg-white z-10 p-4">
+        <SideBar />
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
             <Categories
               categories={categories}
               selectedCategory={selectedCategory}
               onSelect={setSelectedCategory}
             />
           </div>
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+            {videos.map((video) => (
+              <VideoGridItem key={video.id} video={video} />
+            ))}
+          </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
