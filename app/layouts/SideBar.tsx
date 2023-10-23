21ac537988422f2324f4053afe023248eeb45a60
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-10-23 08:35:09 +0600

    Build Sidebar

--- app/layouts/SideBar.tsx
+++ app/layouts/SideBar.tsx
@@ -26,11 +26,20 @@ import { Children, ElementType, ReactNode, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import Button, { buttonStyles } from "../components/Button";
 import { playlists, subscriptions } from "../data/sidebar";
+import { useSidebarContext } from "../contexts/SideBarContext";
+import { PageHeaderFirstSection } from "./PageHeader";
 
 const SideBar = () => {
+  const { isLargeOpen, isSmallOpen, close } = useSidebarContext();
+
   return (
     <>
-      <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+      <aside
+        className={`sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1   ${
+          isLargeOpen ? "lg:hidden" : "lg:flex"
+        } 
+      `}
+      >
         <SmallSidebarItem Icon={Home} title="Home" url="/" />
         <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
         <SmallSidebarItem
@@ -41,7 +50,22 @@ const SideBar = () => {
         <SmallSidebarItem Icon={Library} title="Library" url="/library" />
       </aside>
 
-      <aside className="w-52 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col gap-2 px-2 ">
+      {isSmallOpen && (
+        <div
+          onClick={close}
+          className="lg:hidden fixed z-[999] inset-0 bg-secondary-dark opacity-50"
+        />
+      )}
+
+      <aside
+        className={`w-52 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4  flex-col gap-2 px-2  
+         ${isLargeOpen ? "lg:flex" : "lg:hidden"} 
+        ${isSmallOpen ? "flex z-[999] bg-white max-h-screen " : "hidden"}`}
+      >
+        <div className="lg:hidden pt-2 pb-4 px-2 sticky top-0 bg-white">
+          <PageHeaderFirstSection />
+        </div>
+
         <LargeSidebarSection>
           <LargeSidebarItem isActive IconOrImgUrl={Home} title="Home" url="/" />
           <LargeSidebarItem
--- app/page.tsx
+++ app/page.tsx
@@ -5,30 +5,33 @@ import { categories, videos } from "./data/home";
 import { useState } from "react";
 import { VideoGridItem } from "./components/VideoGridItem";
 import SideBar from "./layouts/SideBar";
+import { SidebarProvider } from "./contexts/SideBarContext";
 
 export default function Home() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
   return (
-    <div className="max-h-screen flex flex-col">
-      <PageHeader />
-      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
-        <SideBar />
-        <div className="overflow-x-hidden px-8 pb-4">
-          <div className="sticky top-0 bg-white z-10 pb-4">
-            <Categories
-              categories={categories}
-              selectedCategory={selectedCategory}
-              onSelect={setSelectedCategory}
-            />
-          </div>
-          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
-            {videos.map((video) => (
-              <VideoGridItem key={video.id} video={video} />
-            ))}
+    <SidebarProvider>
+      <div className="max-h-screen flex flex-col">
+        <PageHeader />
+        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+          <SideBar />
+          <div className="overflow-x-hidden px-8 pb-4">
+            <div className="sticky top-0 bg-white z-10 pb-4">
+              <Categories
+                categories={categories}
+                selectedCategory={selectedCategory}
+                onSelect={setSelectedCategory}
+              />
+            </div>
+            <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+              {videos.map((video) => (
+                <VideoGridItem key={video.id} video={video} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 }
